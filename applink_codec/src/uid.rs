7c2ee284dd42c724e40b22383cbbb090b42a1f10
Igor Valet <igor_valet@hotmail.com>
2023-04-11 11:58:24 +0000

    [codec] Make wizzi_common Uid <-> applink Uid conversion

--- applink_codec/src/uid.rs
+++ applink_codec/src/uid.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, Eq, Hash)]
 pub enum Uid {
-    Dash7(u64),
+    Dash7(wizzi_common::dash7::Uid),
     Vgw(String),
     Unknown(String),
 }
@@ -15,12 +15,12 @@ pub enum UidDash7DecodeError {
 impl Uid {
     pub const VGW_PREFIX: &'static str = "VGW-";
 
-    pub fn parse_dash7(s: &str) -> Result<u64, UidDash7DecodeError> {
+    pub fn parse_dash7(s: &str) -> Result<wizzi_common::dash7::Uid, UidDash7DecodeError> {
         let data: [u8; 8] = hex::decode(s)
             .map_err(|_| UidDash7DecodeError::BadHex)?
             .try_into()
             .map_err(|_| UidDash7DecodeError::BadSize)?;
-        Ok(u64::from_be_bytes(data))
+        Ok(data)
     }
 }
 
@@ -37,10 +37,27 @@ impl From<String> for Uid {
     }
 }
 
+impl From<wizzi_common::dash7::Uid> for Uid {
+    fn from(uid: wizzi_common::dash7::Uid) -> Self {
+        Self::Dash7(uid)
+    }
+}
+
+impl std::convert::TryFrom<Uid> for wizzi_common::dash7::Uid {
+    type Error = ();
+
+    fn try_from(uid: Uid) -> Result<Self, Self::Error> {
+        match uid {
+            Uid::Dash7(uid) => Ok(uid),
+            _ => Err(()),
+        }
+    }
+}
+
 impl std::fmt::Display for Uid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Dash7(uid) => write!(f, "{:016X}", uid),
+            Self::Dash7(uid) => write!(f, "{}", hex::encode_upper(uid)),
             Self::Vgw(uid) => write!(f, "{}{}", Self::VGW_PREFIX, uid),
             Self::Unknown(uid) => write!(f, "{}", uid),
         }
@@ -50,7 +67,7 @@ impl std::fmt::Display for Uid {
 impl std::fmt::Debug for Uid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Dash7(uid) => write!(f, "Dash7({:016X})", uid),
+            Self::Dash7(uid) => write!(f, "Dash7({})", hex::encode_upper(uid)),
             Self::Vgw(uid) => write!(f, "Vgw({})", uid),
             Self::Unknown(uid) => write!(f, "Unknown({})", uid),
         }
