eb6898c6cf137acd00d9fd4c52f50719e4e08fbb
jeremie <jeremie@wizzilab.com>
2023-06-09 18:13:44 +0200

    [applink_xml] copy clone

--- applink_xml/src/apps/common.rs
+++ applink_xml/src/apps/common.rs
@@ -3,7 +3,7 @@ use num_enum::{IntoPrimitive, TryFromPrimitive};
 use serde::{Deserialize, Serialize};
 
 // {"sys_op_mode"=>0, "sys_boot_cause"=>"41", "sys_assert_count"=>1, "sys_last_assert"=>2199215613, "sys_last_assert_arg"=>0}
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Copy, Clone, Deserialize, Serialize)]
 pub struct WmSys {
     pub sys_op_mode: WmSysOpMode,
     #[serde(rename = "sys_boot_cause")]
@@ -17,7 +17,7 @@ pub struct WmSys {
     pub last_assert_arg: u32,
 }
 
-#[derive(Debug, Deserialize, Serialize, IntoPrimitive, TryFromPrimitive)]
+#[derive(Debug, Copy, Clone, Deserialize, Serialize, IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
 #[serde(try_from = "serde_json::Value")]
 pub enum WmSysOpMode {
--- applink_xml/src/modem/v6_3.rs
+++ applink_xml/src/modem/v6_3.rs
@@ -2,7 +2,7 @@ use crate::*;
 use serde::{Deserialize, Serialize};
 
 //{"code"=>"30314243353043373030303031303031", "hwv"=>2118404, "fwid"=>144, "fwmaj"=>6, "fwmin"=>3, "fwp"=>300, "fwh"=>2144575652, "maxsize"=>76544}
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct ModemRevision {
     pub code: String,
     pub hwv: u32,
@@ -17,7 +17,7 @@ pub struct ModemRevision {
 impl_xml!(ModemRevision, 2, "modem_version");
 
 //{"code"=>"30314243353043374646303030303146", "hwv"=>3346433, "fwid"=>131, "fwmaj"=>0, "fwmin"=>6, "fwp"=>115, "fwh"=>477821715, "maxsize"=>163840}
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct HostRevision {
     pub code: String,
     pub hwv: u32,
@@ -32,7 +32,7 @@ pub struct HostRevision {
 impl_xml!(HostRevision, 65, "host_version");
 
 // {"last_assert"=>0, "last_assert_arg"=>0, "assert_count"=>0, "host_present"=>1, "rst_cause"=>80, "active_itf"=>1, "active_itf_fields"=>{"hst"=>1, "com"=>0, "dbg"=>0, "d7a"=>0, "lwan"=>0}}
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Copy, Clone, Deserialize, Serialize)]
 pub struct WmDebug {
     pub last_assert: u32,
     pub last_assert_arg: u32,
@@ -46,7 +46,7 @@ pub struct WmDebug {
     pub active_itf_fields: Option<ActiveItf>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Copy, Clone, Deserialize, Serialize)]
 pub struct ActiveItf {
     #[serde(deserialize_with = "de_boolean")]
     pub hst: bool,
